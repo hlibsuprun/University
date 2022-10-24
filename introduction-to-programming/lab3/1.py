fd2b1f7d776b303670f1840d2fbca4b842796a96
glebsuprun <hlibsuprun@gmail.com>
2022-10-24 22:47:08 +0200

    lab3 zad1-3

---
+++ introduction-to-programming/lab3/1.py
@@ -0,0 +1,11 @@
+first_number = int(input("Wprowadź pierwszą liczbę:"))
+second_number = int(input("Wpisz drugą liczbę:"))
+
+numbers_string = ""
+current_number = min(first_number, second_number)
+
+while current_number <= max(first_number, second_number):
+    numbers_string += f" {current_number}"
+    current_number += 1
+
+print(numbers_string)
---
+++ introduction-to-programming/lab3/3.py
@@ -0,0 +1,4 @@
+while True:
+    number = int(input("Wpisz liczbę całkowitą:"))
+    if number > 0: continue
+    if number < 0: break
